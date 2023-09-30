efd0bf58e4e6dfad7c712749eb2a603643c4f63e
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2023-09-30 15:58:52 -0300

    sonartf deploy

---
+++ aws/sonar/sonar.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_security_group" "sonarqube_sg" {
+  name        = "sonarqube-sg"
+  description = "Security Group for SonarQube"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "sonarqube_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
+  instance_type = "t2.medium"
+
+  key_name = "diego-aws" 
+  security_groups = [aws_security_group.sonarqube_sg.name]
+
+  tags = {
+    Name = "SonarQube"
+    owner = "diego"
+    created_by = "tf_automation"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y java-11-openjdk-devel wget unzip",
+      "wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.2.1.78527.zip",
+      "unzip sonarqube-10.2.1.78527.zip",
+      "sudo mv sonarqube-10.2.1.78527 /opt/sonarqube",
+      "echo '[Unit]' | sudo tee /etc/systemd/system/sonarqube.service",
+      "echo 'Description=SonarQube service' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo 'After=syslog.target network.target' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo '' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo '[Service]' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo 'Type=forking' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo 'ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo 'ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo 'User=ec2-user' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo 'Restart=always' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo '' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo '[Install]' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "echo 'WantedBy=multi-user.target' | sudo tee -a /etc/systemd/system/sonarqube.service",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable sonarqube",
+      "sudo systemctl start sonarqube"
+    ]
+  }
+}
+
+terraform {
+  backend "s3" {
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.sonarqube_instance.id
+}
+
+# resource "aws_ebs_volume" "extra_storage" {
+#   availability_zone = aws_instance.sonarqube_instance.availability_zone
+#   size              = 50
+
+#   tags = {
+#     InstanceID = aws_instance.sonarqube_instance.id
+#   }
+# }
